0ec6ff12a947fb149e95ce75406f244b6655f1e3
taanguyen <taa.nguyen@gmail.com>
2020-11-01 22:33:36 -0500

    display only top 5 frequently recommended books

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,34 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from app import app
 from app.forms import InputLeadersForm
 from leader import Leader
+from BookSystem import BookSystem
+from scraper import Scraper
+from book import Book
 
-@app.route('/')
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/',methods = ['POST', 'GET'])
+@app.route('/index', methods=['POST', 'GET'])
 def index():
-    form = InputLeadersForm(request.form)
+    form = InputLeadersForm()
     if request.method == 'POST':
-            leaders = request.form['leaders']
-            print(leaders)
-           # scraper = Scraper(leaders)
-    return render_template('index.html', form=form)
+        return redirect(url_for('results', leaders = form.leaders.data))
+    return render_template('index.html', form = form)
 
-@app.route('/results', methods=['GET', 'POST'])
-def results():
-    form = InputLeadersForm(request.form)
+@app.route('/results/<leaders>', methods=['GET', 'POST'])
+def results(leaders):
+    form = InputLeadersForm()
     if request.method == 'POST':
-            userInput = request.form['leaders']
-            # split input by semicolon
-            names = userInput.split(";")
+        return redirect(url_for('index'))
+    else:
+        if leaders:
+            names = leaders.split(";")
             leaders = []
             for name in names:
-                if name:
+                if name: 
                     name = name.lower().strip()
-                    leaders.append(Leader(name))
-    return render_template('results.html', form=form)
+                    leaders.append(name)
+            bookUrlsWithFreq = Scraper.bookUrlsWithFreq(leaders)
+            bookurls = list(bookUrlsWithFreq.keys())[:5]
+            books = [Scraper.bookFromUrl(bookurl) for bookurl in bookurls if bookurl]
+            return render_template('results.html', form = form, books = books)
+    return render_template('results.html', form=form, leaders=None)
